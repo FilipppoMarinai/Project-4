00c2ce7f7433b31d6f68a6f4e9bf77c14ed110d6
lai2-24 <lai2-24@itismeucci.com>
2023-12-13 13:43:37 +0100

    modifiche

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,11 @@
 package com.example;
 
 import java.io.BufferedReader;
+import java.io.DataOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -20,25 +24,29 @@ public class App
             while(true){
                 Socket socket = server.accept();
                 BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
                 String message;
                 message = in.readLine();
                 String[] stringhe = message.split(" ");
                 String path = stringhe[1].substring(1);
-            
 
                 while(!message.isEmpty()){
                     System.out.println(message + "\n");
                     message = in.readLine();
                 }
 
-                File document = new File(path);
+                File file = new File(path);
                 
                 try{
-                    if(document.exists()){
-                        System.out.println("Il file esiste\n");
+                    if(file.exists()){
+                        sendBinaryFile(socket, file);
                     }
                     else{
-                        System.out.println("Il file non esiste\n");
+                        String stringa = "Il file non esiste";
+                        out.writeBytes("HTTP/1.1 404 Not Found\n");
+                        out.writeBytes("Content-Length: " + stringa.length() + "\n");
+                        out.writeBytes("Content-Type: text/plain\n");
+                        out.writeBytes("\n");
                     }
                 }
                 catch(Exception e){
@@ -50,5 +58,46 @@ public class App
         catch(Exception e){
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void sendBinaryFile(Socket socket, File file) throws IOException{
+        DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+        out.writeBytes("HTTP/1.1 200 OK\n");
+        out.writeBytes("Content-Length: " + file.length() + "\n");
+
+        out.writeBytes("Content-Type: " + chooseType(file) + "\n");
+        
+        out.writeBytes("\n");
+        InputStream input = new FileInputStream(file);
+        byte[] buf = new byte[8192];
+        int n;
+        while((n = input.read(buf)) != -1){
+            out.write(buf, 0, n);
+        }
+        input.close();
+    }
+
+    private static String chooseType(File file){
+        String contentType = "";
+        String path = file.getName();
+        String[] stringhe = path.split("\\.");
+        String extension = stringhe[1];
+
+        switch (extension) {
+            case "png":
+                contentType = "image/png";
+                break;
+            case "html":
+                contentType = "text/html";
+                break;
+            case "css":
+                contentType = "text/css";
+                break;
+            case "js":
+                contentType = "application/javascript";
+                break;
+        }
+
+        return contentType;
     }
 }
